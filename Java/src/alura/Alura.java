0853b84c01a77667e1e9cfa667f0f16407a77302
Pedro K. L <pedro.karasudani@hotmail.com>
2023-05-05 20:15:20 -0300

    Adicionado legenda no codigo de quem fez cod1 e cod2

--- Java/src/alura/Alura.java
+++ Java/src/alura/Alura.java
@@ -4,7 +4,7 @@ public class Alura {
     public static void main(String[] args) {
         
         
-        // aula de fatorial
+        // aula de fatorial codigo feito pelo pedro
 //        int resultado = 1;
 //        int i = 1;
 //        for(int x = 1; x<= 10; x++){
@@ -18,6 +18,8 @@ public class Alura {
 //            }
 //        }
     
+
+       // codigo pelo professor(mais facil)
         int fatorial = 1;
         for (int i = 1; i < 11; i++) {
             fatorial *= i;
