1a8efc14ce17d6abe65f456b89016b1b1c217e3a
Pedro K. L <pedro.karasudani@hotmail.com>
2023-05-09 20:14:19 -0300

    Inicio aula 5

--- Java/src/alura/CriaConta.java
+++ Java/src/alura/CriaConta.java
@@ -1,28 +1,12 @@
-
 package alura;
 
 public class CriaConta {
-    public static void main(String[] args) {
-        Conta contaPedro = new Conta();
-        contaPedro.deposito(200);          
 
-        
-        
-        
-        Conta contaBarbara = new Conta();
-        contaBarbara.deposito(1000.25);
-        
-        if(contaBarbara.transfere(500, contaPedro)){
-            System.out.println("Transferencia feita com sucesso");
-        } else {
-            System.out.println("Faltou dinheiro");
-            
-        }
-        
-        System.out.println(contaPedro.saldo);
-        System.out.println(contaBarbara.saldo);
-        
+    public static void main(String[] args) {
+        Conta conta = new Conta();
+        conta.deposito(100);
+        System.out.println(conta.saca(200));
+        System.out.println(conta.saldo);
     }
-    
-        
+
 }
